0ff8e34ca71087a56c8ff9d719ff612be6c1a184
Imran Ali Namazi <imran@joyfulearth.org>
2026-09-05 17:49:18 +0530

    domain

--- domain.php
+++ domain.php
@@ -1,11 +1,11 @@
 <?php
 domain::add('aztras', __DIR__, true, new domain([
 		'folder' => 'aztras/',
-		'heading' => 'AZ',
+		'heading' => 'Aztra\s',
 		'local' => 'http://localhost/aztras/%subfol%/%site%/',
-		'live' => 'https://%site%.aztras.org/',
+		'live' => 'https://%site%.aztras.com/',
 		'local-base' => 'http://localhost/aztras/%subfol%/',
-		'live-base' => 'https://%subfol%.aztras.org/',
+		'live-base' => 'https://%subfol%.aztras.com/',
 	],
 	['%folder%www', '%folder%businesses/realtors'],
 	[
